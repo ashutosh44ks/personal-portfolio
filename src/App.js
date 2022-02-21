d75a65e00afdcbeb13da9986a50b671a4d9758db
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2022-02-22 01:52:18 +0530

    update file content locations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import Layout from './components/Layout'
-import Intro from "./components/Intro"
-import Parallax from './components/Parallax'
-import Main from './components/Main'
+import Layout from "./components/Layout";
+import Intro from "./components/Intro";
+import Parallax from "./components/Parallax";
+import Main from "./components/Main";
+import "./components/stylesheets/Layout.css";
+import "./components/stylesheets/Main.css"
+
 function App() {
   return (
     <div className="App">
-      <Layout/>
-      <Intro/>
-      <Parallax type="parallax-img" tag="pic-1"/>
-      <Main/>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo tempore provident nam. Velit quae temporibus libero rerum odio in a, accusantium ex illum, ducimus debitis sunt quia ipsa harum nostrum.
-      
+      <Layout />
+      <Intro />
+      <Parallax type="parallax-img" tag="pic-1" />
+      <Main />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo tempore
+      provident nam. Velit quae temporibus libero rerum odio in a, accusantium
+      ex illum, ducimus debitis sunt quia ipsa harum nostrum.
     </div>
   );
 }

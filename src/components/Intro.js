bf8de9215543512a75df036a354f46ef70ef7e13
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2022-02-22 01:34:25 +0530

    update intro
    Now it's not a "jugaad"
    It's well written CSS

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,9 +3,7 @@ import React from "react";
 const Intro = () => {
   return (
     <div id="intro-container">
-      <div className="intro intro-left"></div>
       <div id="intro-text">NAMASTE.</div>
-      <div className="intro intro-right"></div>
     </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,14 +6,17 @@ const Sidebar = () => {
     const sideBtn = document.querySelector("#navbar-toggle");
     const sideBar = document.querySelector("#sidebar-container");
     const sideMenu = document.querySelector("#navbar-menu");
+    const introContainer = document.querySelector('#intro-container');
     sideBtn.addEventListener("click", () => {
       if (toggle === "Menu") {
-        sideBar.style.width = "60em";
+        sideBar.style.width = "80vw";
         sideMenu.classList.remove("hide");
+        introContainer.classList.add("theme-1");
         setToggle("X");
       } else if (toggle === "X") {
         sideBar.style.width = "2em";
         sideMenu.classList.add("hide");
+        introContainer.classList.remove("theme-1");
         setToggle("Menu");
       }
     });
